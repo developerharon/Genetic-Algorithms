fa09123200cd4b71bd76ae8ba0b9466e28c50ed2
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-12-09 17:38:14 +0300

    Add project files.

---
+++ GeneticAlgorithm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeneticAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "to be or not to be that is the question";
+            Console.WriteLine(s);
+        }
+    }
+}
